c598c6c2bc16babe033bc74d726e8803bbd20502
dingjiahao00 <2684047674@qq.com>
2021-01-03 14:41:38 +0800

    最终提交

--- app/src/main/java/cn/edu/togethorapp/ui/home/HomeFragment.kt
+++ app/src/main/java/cn/edu/togethorapp/ui/home/HomeFragment.kt
@@ -1,31 +1,77 @@
 package cn.edu.togethorapp.ui.home
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+//import androidx.gridlayout.widget.GridLayout;
 import android.widget.TextView
+import androidx.core.view.children
+
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import cn.edu.togethorapp.ui.home.model.CardMatchingGame
 import cn.edu.togethorapp.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment() , View.OnClickListener{
 
     private lateinit var homeViewModel: HomeViewModel
+
+    lateinit var game: CardMatchingGame
+    val  cardButtons= mutableListOf<Button>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
         return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        for (v in gridLayout.children){
+            if (v is Button){
+                v.setOnClickListener (this)
+                cardButtons.add(v)
+            }
+        }
+        game = CardMatchingGame(cardButtons.count())
+        updateUI()
+        button_reset.setOnClickListener {
+            game= CardMatchingGame(cardButtons.count())
+            updateUI()
+        }
+    }
+
+    fun updateUI(){
+        for (button in cardButtons) {
+            val index = cardButtons.indexOf(button)
+            val card =game.cardAtIndex(index)
+            button.isEnabled=!card.isMatched
+            if (card.isChosen){
+                button.text = card.toString()
+                button.setBackgroundColor(Color.WHITE)
+            }else{
+                button.text=""
+                button.setBackgroundResource(R.drawable.yuzhou)
+            }
+        }
+        score.text= String.format("%s%d","Score",game.score)
+    }
+
+    override fun onClick(v: View?) {
+        if (v is Button){
+            val index =cardButtons.indexOf(v)
+            game.chooseCardAtIndex(index)
+            updateUI()
+        }
+    }
+
 }
--- app/src/main/java/cn/edu/togethorapp/ui/notifications/CityFragment.kt
+++ app/src/main/java/cn/edu/togethorapp/ui/notifications/CityFragment.kt
@@ -1,5 +1,6 @@
 package cn.edu.togethorapp.ui.notifications
 
+import android.app.Application
 import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,18 +9,19 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import cn.edu.togethorapp.R
+import cn.edu.togethorapp.ui.notifications.CityViewModel
 import cn.edu.togethorapp.ui.watch.WatchViewModel
 import kotlinx.android.synthetic.main.fragment_notifications.*
-import kotlinx.android.synthetic.main.watch_fragment.*
-//import cn.edu.togethorapp.activitytest.*;
+
 
 class CityFragment : Fragment() {
 
-    private lateinit var viewModel: CityViewModel
+    lateinit var viewModel: CityViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,30 +31,19 @@ class CityFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_notifications, container, false)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-////        setContentView(R.layout.fragment_notifications)
-////        viewModel=ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory(application)).get(CityViewModel::class.java)
-//        viewModel = ViewModelProviders.of(this).get(CityViewModel::class.java)
-//        viewModel.cities.observe(this, Observer {
-//            val cities=it
-//            val adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1,cities)
-//            listView.adapter = adapter
-//            listView.setOnItemClickListener { _, _, position, _ ->
-//                val cityCode = cities[position].city_code
-//                val intent = Intent(this,Main2Activity::class.java)
-//                intent.putExtra("city_code", cityCode)
-//                startActivity(intent)
-//            }
-//        })
+        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.fragment_notifications)
+//        viewModel=ViewModelProvider(this , ViewModelProvider.AndroidViewModelFactory(application)).get(CityViewModel::class.java)
+//        viewModel=ViewModelProvider(this , ViewModelProvider.AndroidViewModelFactory(Application())).get(CityViewModel::class.java)
+        viewModel = ViewModelProviders.of(this).get(CityViewModel::class.java)
+        viewModel.cities.observe(requireActivity(), Observer {
 
-//        viewModel=ViewModelProvider(this,ViewModelProvider.AndroidViewModelFactory(appliction)).get(CityViewModel::class.java)
-        viewModel.cities.observe(viewLifecycleOwner, Observer {
             val cities=it
-            val adapter = ArrayAdapter(viewLifecycleOwner,android.R.layout.simple_list_item_1,cities)
+            val adapter = ArrayAdapter(requireActivity(),android.R.layout.simple_list_item_1,cities)
             listView.adapter = adapter
             listView.setOnItemClickListener { _, _, position, _ ->
                 val cityCode = cities[position].city_code
-                val intent = Intent(this, Main2Activity::class.java)
+                val intent = Intent(requireActivity(), Main2Activity::class.java)
                 intent.putExtra("city_code", cityCode)
                 startActivity(intent)
             }
